89eb35e449d4b63802baf655dcdcf5c30e2266dd
Samuel Cabral <samuelcabral.mail@gmail.com>
2024-03-03 00:09:09 -0300

    feat: craate footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,8 +1,12 @@
+import { Logo } from './Logo'
+
 export function Footer() {
   return (
-    <footer>
-      <div>
-        <p>&copy; 2021 AgendaCraft</p>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Logo />
+
+        <p>&copy; 2021 AgendaCraft. All rights reserved.</p>
       </div>
     </footer>
   )
